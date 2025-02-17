0c1fc448f48698e85ad0f55fc4f240362bac78fe
Cade Sayner <Cade.Sayner@bbd.co.za>
2025-02-17 15:44:13 +0200

    Add user stored proc fixed

--- migrations/V4__POPULATE_DEFAULTS.sql
+++ migrations/V4__POPULATE_DEFAULTS.sql
@@ -199,21 +199,21 @@ EXEC InsertDialog 7, 1, '*Thumbs up awkwardly*', 2;
 EXEC InsertDialog 8, 7, 'Sir, you must have a ticket.', 1;
 EXEC InsertDialog 8, 1, '*Holds up a completely useless ticket stub*', 2;
 
-EXEC InsertUser 'beanfan99', 'beanfan99@email.com', '2025-02-17 10:00:00';
-EXEC InsertUser 'irmaziegler', 'irma.ziegler@email.com', '2025-02-17 10:05:00';
-EXEC InsertUser 'comedylover', 'comedylover@email.com', '2025-02-17 10:10:00';
-EXEC InsertUser 'silentmovieguy', 'silentmovieguy@email.com', '2025-02-17 10:15:00';
-EXEC InsertUser 'laughoutloud', 'lolbean@email.com', '2025-02-17 10:20:00';
-EXEC InsertUser 'retroTVfan', 'retrotv@email.com', '2025-02-17 10:25:00';
-EXEC InsertUser 'rowanatkinsonfan', 'rowanbean@email.com', '2025-02-17 10:30:00';
-EXEC InsertUser 'classiccomedy', 'classiccomedy@email.com', '2025-02-17 10:35:00';
-EXEC InsertUser 'slapsticklover', 'slapstick@email.com', '2025-02-17 10:40:00';
-EXEC InsertUser 'tvbinger', 'tvbinger@email.com', '2025-02-17 10:45:00';
-EXEC InsertUser 'mrbeanfanatic', 'beanfanatic@email.com', '2025-02-17 10:50:00';
-EXEC InsertUser 'britishhumour', 'britishhumour@email.com', '2025-02-17 10:55:00';
-EXEC InsertUser 'lolmoments', 'lolmoments@email.com', '2025-02-17 11:00:00';
-EXEC InsertUser 'teddyfan', 'teddyfan@email.com', '2025-02-17 11:05:00';
-EXEC InsertUser 'nostalgicviewer', 'nostalgic@email.com', '2025-02-17 11:10:00';
+EXEC InsertUser 'beanfan99', 'beanfan99@email.com', '$2b$12$ZfJBo4oeb/9VaTtPaIbRrOBB7gTpo5a1eABME1TV6oXHQ0Po.3c0m', '2025-02-17 10:00:00';
+EXEC InsertUser 'irmaziegler', 'irma.ziegler@email.com', '$2b$12$ZfJBo4oeb/9VaTtPaIbRrOBB7gTpo5a1eABME1TV6oXHQ0Po.3c0m', '2025-02-17 10:05:00';
+EXEC InsertUser 'comedylover', 'comedylover@email.com', '$2b$12$ZfJBo4oeb/9VaTtPaIbRrOBB7gTpo5a1eABME1TV6oXHQ0Po.3c0m', '2025-02-17 10:10:00';
+EXEC InsertUser 'silentmovieguy', 'silentmovieguy@email.com','$2b$12$ZfJBo4oeb/9VaTtPaIbRrOBB7gTpo5a1eABME1TV6oXHQ0Po.3c0m', '2025-02-17 10:15:00';
+EXEC InsertUser 'laughoutloud', 'lolbean@email.com','$2b$12$ZfJBo4oeb/9VaTtPaIbRrOBB7gTpo5a1eABME1TV6oXHQ0Po.3c0m', '2025-02-17 10:20:00';
+EXEC InsertUser 'retroTVfan', 'retrotv@email.com', '$2b$12$ZfJBo4oeb/9VaTtPaIbRrOBB7gTpo5a1eABME1TV6oXHQ0Po.3c0m','2025-02-17 10:25:00';
+EXEC InsertUser 'rowanatkinsonfan', 'rowanbean@email.com','$2b$12$ZfJBo4oeb/9VaTtPaIbRrOBB7gTpo5a1eABME1TV6oXHQ0Po.3c0m', '2025-02-17 10:30:00';
+EXEC InsertUser 'classiccomedy', 'classiccomedy@email.com','$2b$12$ZfJBo4oeb/9VaTtPaIbRrOBB7gTpo5a1eABME1TV6oXHQ0Po.3c0m', '2025-02-17 10:35:00';
+EXEC InsertUser 'slapsticklover', 'slapstick@email.com','$2b$12$ZfJBo4oeb/9VaTtPaIbRrOBB7gTpo5a1eABME1TV6oXHQ0Po.3c0m', '2025-02-17 10:40:00';
+EXEC InsertUser 'tvbinger', 'tvbinger@email.com','$2b$12$ZfJBo4oeb/9VaTtPaIbRrOBB7gTpo5a1eABME1TV6oXHQ0Po.3c0m', '2025-02-17 10:45:00';
+EXEC InsertUser 'mrbeanfanatic', 'beanfanatic@email.com','$2b$12$ZfJBo4oeb/9VaTtPaIbRrOBB7gTpo5a1eABME1TV6oXHQ0Po.3c0m', '2025-02-17 10:50:00';
+EXEC InsertUser 'britishhumour', 'britishhumour@email.com','$2b$12$ZfJBo4oeb/9VaTtPaIbRrOBB7gTpo5a1eABME1TV6oXHQ0Po.3c0m', '2025-02-17 10:55:00';
+EXEC InsertUser 'lolmoments', 'lolmoments@email.com','$2b$12$ZfJBo4oeb/9VaTtPaIbRrOBB7gTpo5a1eABME1TV6oXHQ0Po.3c0m', '2025-02-17 11:00:00';
+EXEC InsertUser 'teddyfan', 'teddyfan@email.com','$2b$12$ZfJBo4oeb/9VaTtPaIbRrOBB7gTpo5a1eABME1TV6oXHQ0Po.3c0m', '2025-02-17 11:05:00';
+EXEC InsertUser 'nostalgicviewer', 'nostalgic@email.com','$2b$12$ZfJBo4oeb/9VaTtPaIbRrOBB7gTpo5a1eABME1TV6oXHQ0Po.3c0m', '2025-02-17 11:10:00';
 
 
 
